22e42c4fcf3931f9d63fe7f4d5f4f4b4bb61db68
relytnroh <relytnroh@gmail.com>
2022-07-18 13:34:53 -0700

    Weather api test

--- demo/src/main/java/com/example/demo/controllers/WeatherController.java
+++ demo/src/main/java/com/example/demo/controllers/WeatherController.java
@@ -1,18 +1,16 @@
 package com.example.demo.controllers;
 
-import com.RFDSpring.entities.Account;
-import com.RFDSpring.services.AccountService;
+
 import com.example.demo.services.WeatherService;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class WeatherController {
     private WeatherService weatherService;
-    private String weatherBaseURL =  "https://api.openweathermap.org/data/3.0/onecall?";
-    private String apiKEY = "8692a5fdc5525d6d03aa2f9683c1666f";
     public WeatherController(WeatherService weatherService) {
         this.weatherService = weatherService;
 
@@ -20,13 +18,8 @@ public class WeatherController {
 
     @CrossOrigin(origins = "*")
     @GetMapping("/weather/{location}")
-    public List getAccounts(@PathVariable String location){
+    public String getAccounts(@PathVariable String location) throws IOException {
         return this.weatherService.getWeather(location);
-    }
-
-    @GetMapping("/weather")
-    public List createAccount() {
-        return this.weatherService.getWeatherAll();
     }
 
 }
--- demo/src/main/java/com/example/demo/services/WeatherService.java
+++ demo/src/main/java/com/example/demo/services/WeatherService.java
@@ -2,20 +2,27 @@ package com.example.demo.services;
 
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
 
 @Service
 public class WeatherService {
+    private String weatherBaseURL =  "http://api.weatherapi.com/v1/current.json?key=";
+    private String apiKEY = "36b2363860ac4636b19202601221807";
 
     public WeatherService() {
     }
 
-    public List<WeatherObject> findByLocation(String location) {
-//        return WeatherService.findbyLocation(location);
-    }
-
-    public List<WeatherObject> getWeatherAll() {
-        //
-        return null
+    public String getWeather(String location) throws IOException {
+        URL url = new URL(weatherBaseURL+apiKEY+ " &q="+location+"&aqi=no");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("accept", "application/json");
+        InputStream responseStream = con.getInputStream();
+        return responseStream.toString();
     }
 }
